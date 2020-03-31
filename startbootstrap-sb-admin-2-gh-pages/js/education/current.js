a28047b08d7c8417cb554f29afeca24707968575
Natalie Wang <n.wang@berkeley.edu>
2020-03-30 19:02:35 -0500

    working current top cards on education.html

--- startbootstrap-sb-admin-2-gh-pages/js/education/current.js
+++ startbootstrap-sb-admin-2-gh-pages/js/education/current.js
@@ -42,7 +42,7 @@ d3.csv("/data/covid_19_data.csv", function(data) {
 		var svg = d3.select(classes[j])
 			.append("svg")
 			.attr("width", 150)
-			.attr("height", 60);
+			.attr("height", 50);
 
 		var text = svg.selectAll("text")
 			.data(circleData)
@@ -50,14 +50,41 @@ d3.csv("/data/covid_19_data.csv", function(data) {
 			.append("text");
 
 		var textLabels = text
-	     .attr("x", 0)
-	     .attr("y", 50)
+	     .attr("x", 10)
+	     .attr("y", 40)
 	     .text(formatNum(counts[j]))
 	     .attr("font-family", "nunito")
 	     .attr("font-size", "30px")
 	     .attr("font-weight", "bold")
 	     .attr("fill", "dark-gray");
 	}
+
+	// update days
+	var today = new Date()
+	var first = new Date("01/22/2020")
+	var difference = today.getTime() - first.getTime()
+	var days = Math.round(difference/(1000 * 60  * 60  * 24), 2)
+
+
+	var svg = d3.select("#days")
+			.append("svg")
+			.attr("width", 150)
+			.attr("height", 50);
+
+		var text = svg.selectAll("text")
+			.data(circleData)
+			.enter()
+			.append("text");
+
+		var textLabels = text
+	     .attr("x", 10)
+	     .attr("y", 40)
+	     .text(days)
+	     .attr("font-family", "nunito")
+	     .attr("font-size", "30px")
+	     .attr("font-weight", "bold")
+	     .attr("fill", "dark-gray");
+
 
 	
 });
